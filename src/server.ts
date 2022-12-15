868193850e1efa8ecbaad5099422c50adca5a572
William Fu <williamfu37@gmail.com>
2022-12-15 17:08:53 +0700

    feat: add morgan

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv"
 import express from "express"
 import cors from "cors"
+import morgan from "morgan"
 import conn from "./data"
 
 import user from "./routes/user"
@@ -13,6 +14,7 @@ async function main() {
   const port = process.env.PORT || 3000
 
   app.use(cors())
+  app.use(morgan("dev"))
   app.use(express.json())
   app.use(express.urlencoded({ extended: false }))
 
